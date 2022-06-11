f4e34c3b260c58344fad391a5aaecce59ea47b48
schelen01 <schelengrossel@gmail.com>
2022-06-11 17:10:26 -0300

    Primeiro Commit

---
+++ src/LojaBrinquedo.java
@@ -0,0 +1,16 @@
+
+public class LojaBrinquedo {
+
+    private String nome;
+    private String marca;
+    private int classificIndicativa;
+    private double preco;
+    private int estoque;
+
+    public LojaBrinquedo (String nome, String marca){
+        this.nome =nome;
+        this.marca= marca;
+        this.classificIndicativa = classificIndicativa;
+
+    }
+}
